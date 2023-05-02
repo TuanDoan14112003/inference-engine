7e4194a3214024f7eb48ab1c5d64991f01f2163c
kiennguyen2403 <kientqk2403@example.com>
2023-05-02 19:24:37 +1000

    main.py test

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+from propositionalSymbol import PropositionalSymbol
+
+instanceA = PropositionalSymbol("A")
+instanceB = PropositionalSymbol("B")
+instanceC = PropositionalSymbol("C")
+instanceD = PropositionalSymbol("D")
 
 
+listOfSymbols = [instanceA, instanceB, instanceC, instanceD]
+for i in listOfSymbols:
+    print(i.getValue())
 
 
+instanceA.setValue(True)
+instanceB.setValue(False)
+instanceC.setValue(True)
+instanceD.setValue(False)
 
 
+for i in listOfSymbols:
+    print(i.getValue())
 
-readFile("file.txt")
+
