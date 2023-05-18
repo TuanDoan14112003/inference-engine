260df6bc7690ff137a24db237ef889d9963113a0
kiennguyen2403 <kientqk2403@example.com>
2023-05-18 21:32:16 +1000

    adding general testcases for resolution

--- testGenerator.py
+++ testGenerator.py
@@ -7,47 +7,21 @@ class TestGenerator:
         self.symbols = []
         self.clauses = []
 
-    # def generateHornCase(self, filename):
-    #
-    #     self.clauses = []
-    #     self.symbols = []
-    #     self.symbols = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
-    #     for i in range(50):
-    #         self.clauses = []
-    #         for j in range(1, 10):
-    #             clause = ""
-    #             if (random.randint(0, 1) == 0):
-    #
-    #                 clause += random.choice(self.symbols)
-    #                 clause += "&"
-    #                 clause += random.choice(self.symbols)
-    #                 clause += " => "
-    #                 clause += random.choice(self.symbols)
-    #             else:
-    #                 clause += random.choice(self.symbols)
-    #             self.clauses.append(clause)
-    #
-    #         with open(filename+"horn"+str(i)+".txt", "w") as file:
-    #             file.write("TELL\n")
-    #             for clause in self.clauses:
-    #                 file.write(clause)
-    #                 file.write("; ")
-    #             file.write("\n")
-    #             file.write("ASK\n")
-    #             file.write(random.choice(self.symbols))
-
+   
     def generateHornCase(self, filename):
         for i in range(50):
             self.symbols = list(string.ascii_letters[:5])
             clauses = []
             for j in range(7):
-                hornClause = list(set(random.choices(self.symbols,k=random.randint(1,5))))
+                hornClause = list(
+                    set(random.choices(self.symbols, k=random.randint(1, 5))))
                 tail = hornClause[:-1]
                 if len(hornClause) >= 2:
                     head = hornClause[-1]
                     clauseString = "&".join(tail) + " =>" + head
                     clauses.append(clauseString)
-            literal = list(set(random.choices(self.symbols,k=random.randint(1,3))))
+            literal = list(
+                set(random.choices(self.symbols, k=random.randint(1, 3))))
             clauses.extend(literal)
             query = random.choice(self.symbols)
             while query in literal:
@@ -61,34 +35,31 @@ class TestGenerator:
                 file.write("ASK\n")
                 file.write(query)
 
+    def generateGeneralCase(self,  filename, number, maxdepth=10):
+        for j in range(number):
+            print(j)
+            with open(filename+str(j)+".txt", "w") as file:
+                file.write("TELL\n")
+            self.clauses = []
+            self.symbols = {symbol: 1 for symbol in list(
+                string.ascii_lowercase[:7])}
+            symbol = random.choice(list(self.symbols.keys()))
+            for i in range(0, 10):
+                clause = "("
+                firstSymbol = random.choice(list(self.symbols.keys()))
+                secondSymbol = random.choice(list(self.symbols.keys()))
+                while self.symbols[firstSymbol] + self.symbols[secondSymbol] > maxdepth or firstSymbol == secondSymbol:
+                    firstSymbol = random.choice(list(self.symbols.keys()))
+                    secondSymbol = random.choice(list(self.symbols.keys()))
+                depth = self.symbols[firstSymbol] + self.symbols[secondSymbol]
 
+                if (random.randint(0, 1) == 0):
+                    firstSymbol = "~"+firstSymbol
 
-
-
-
-
-
-
-
-
-    def generateGeneralCase(self):
-
-        for i in range(1, 10):
-            clause = ""
-            if (random.randint(0, 1) == 0):
-                for j in range(1, 2):
-                    clause += random.choice(string.ascii_lowercase)
-                    operator = random.randint(0, 3)
-                    if operator == 0:
-                        clause += "&"
-                    elif operator == 1:
-                        clause += "||"
-                    elif operator == 2:
-                        clause += "=>"
-                    elif operator == 3:
-                        clause += "<=>"
-                clause += random.choice(string.ascii_lowercase)
-                operator = random.randint(0, 3)
+                if (random.randint(0, 1) == 0):
+                    secondSymbol = "~"+secondSymbol
+                clause += firstSymbol
+                operator = random.randint(0, 2)
                 if operator == 0:
                     clause += "&"
                 elif operator == 1:
@@ -97,43 +68,33 @@ class TestGenerator:
                     clause += "=>"
                 elif operator == 3:
                     clause += "<=>"
-                clause += random.choice(string.ascii_lowercase)
-            else:
-                clause += random.choice(string.ascii_lowercase)
+                clause += secondSymbol
+                clause += ")"
+                self.symbols.update({clause: depth})
 
-            if (len(clause) > 1):
-                if (random.randint(0, 1) == 0):
-                    if (clause[-4] == "=" or clause[-4] == "|") and clause[-5] != "<":
-                        clause += ")"
-                        clause = clause[:-5]+"("+clause[-5:]
-                    elif clause[-4] == "=" and clause[-5] != "<":
-                        clause += ")"
-                        clause = clause[:-6]+"("+clause[-6:]
-            self.clauses.append(clause)
+                with open(filename+str(j)+".txt", "a") as file:
+                    file.write(clause)
+                    file.write(";")
+            with open(filename+str(j)+".txt", "a") as file:
+                file.write("\n")
+                file.write("ASK\n")
+                file.write(symbol)
 
-        with open("general.txt", "w") as file:
-            file.write("TELL\n")
-            for clause in self.clauses:
-                file.write(clause)
-                file.write("; ")
-            file.write("\n")
-            file.write("ASK\n")
-            file.write(random.choice(string.ascii_lowercase))
 
     def generateGeneralLogic(self, filename, number, maxdepth=10):
         with open(filename, "w") as file:
             file.write("")
         self.clauses = []
-        self.symbols = {symbol: 1 for symbol in list(string.ascii_lowercase[:7])}
+        self.symbols = {symbol: 1 for symbol in list(
+            string.ascii_lowercase[:7])}
         for i in range(0, number):
             clause = "("
             firstSymbol = random.choice(list(self.symbols.keys()))
             secondSymbol = random.choice(list(self.symbols.keys()))
-            while self.symbols[firstSymbol] +  self.symbols[secondSymbol] > maxdepth or firstSymbol == secondSymbol:
+            while self.symbols[firstSymbol] + self.symbols[secondSymbol] > maxdepth or firstSymbol == secondSymbol:
                 firstSymbol = random.choice(list(self.symbols.keys()))
                 secondSymbol = random.choice(list(self.symbols.keys()))
             depth = self.symbols[firstSymbol] + self.symbols[secondSymbol]
-            
 
             if (random.randint(0, 1) == 0):
                 firstSymbol = "~"+firstSymbol
@@ -152,13 +113,11 @@ class TestGenerator:
                 clause += "<=>"
             clause += secondSymbol
             clause += ")"
-            self.symbols.update({clause:depth})
-            
-            
+            self.symbols.update({clause: depth})
+
             with open(filename, "a") as file:
                 file.write(clause)
                 file.write("\n")
-        print(self.symbols)
 
 
 if __name__ == "__main__":
@@ -166,4 +125,5 @@ if __name__ == "__main__":
     # test1.generateHornCase()
     import sys
     test2 = TestGenerator()
-    test2.generateHornCase("UnitTest/testcases/")
+    test2.generateGeneralCase(
+        "UnitTest/testcases/resolution/resolution", 50, 10)
